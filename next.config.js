649f3961b24e6f8822da9566f29e5d3949369815
Al Murad Uzzaman <murad.themefisher@gmail.com>
2024-12-16 22:35:08 +0600

    next-on-pages removed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
 const config = require("./src/config/config.json");
 
 /** @type {import('next').NextConfig} */
@@ -16,16 +15,5 @@ const nextConfig = {
     ],
   },
 };
-
-// Use an async IIFE to handle the `await`
-if (process.env.NODE_ENV === "development") {
-  (async () => {
-    try {
-      await setupDevPlatform();
-    } catch (err) {
-      console.error("Error setting up dev platform:", err);
-    }
-  })();
-}
 
 module.exports = nextConfig;
